ca49ea881d541fb082deb7a8dce03ace3bef5b5a
ar2-url <57313716+ar2-url@users.noreply.github.com>
2020-11-16 12:45:04 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+  $('#cart').on('click', () => {
+    $('#cartMenu').show();
+    $('.dropdown-menu').on('mouseleave', () => {
+      $('.dropdown-menu').hide();
+    })
+})
+  $('#account').on('click', () => {
+    $('#accountMenu').show();
+    $('.dropdown-menu').on('mouseleave', () => {
+      $('.dropdown-menu').hide();
+    })
+})
+  $('#help').on('click', () => {
+    $('#helpMenu').show();
+    $('.dropdown-menu').on('mouseleave', () => {
+      $('.dropdown-menu').hide();
+    })
+})
+})
